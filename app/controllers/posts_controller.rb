b4892517a413586c4274ce4612b99ee799a47084
nari104 <172471497+nari104@users.noreply.github.com>
2024-11-02 03:41:02 +0000

    [update] 20241102

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,14 +22,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
+    @post = Post.find(params[:id])
   end
 
   def update
+    is_matching_login_user
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "successfully"
+      redirect_to post_path(@post.id)
+    else
+      @posts = Post.all.order(id: :asc)
+      render :edit
+    end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
 
@@ -38,4 +49,12 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :images)
   end
+
+  def is_matching_login_user
+    post = Post.find(params[:id])
+    unless post.user.id == current_user.id
+      redirect_to posts_path
+    end
+  end
+
 end

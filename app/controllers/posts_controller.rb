f7d00de77b93fe39b577bf2ed0ec8de3fdc03db4
nari104 <172471497+nari104@users.noreply.github.com>
2024-11-16 02:18:45 +0000

    first deploy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :images)
+    params.require(:post).permit(:title, :body, images: [])
   end
 
   def is_matching_login_user
